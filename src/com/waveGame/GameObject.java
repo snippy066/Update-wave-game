eef8c93cf415ad0d9ff3cb92acca9475bdcb07aa
Priyanka Thakur <68439548+snippy066@users.noreply.github.com>
2021-10-02 10:14:22 +0530

    Create GameObject.java

---
+++ src/com/waveGame/GameObject.java
@@ -0,0 +1,58 @@
+package com.waveGame;
+
+import java.awt.Graphics;
+import java.awt.Rectangle;
+
+public abstract class GameObject {
+
+	protected int x,y;
+	protected ID id;
+	protected int spdX,spdY;
+	
+	public GameObject(int x,int y,ID id) {
+		this.x=x;
+		this.y=y;
+		this.id=id;
+	}
+	
+	public abstract void tick();
+	public abstract void render(Graphics g);
+	public abstract Rectangle getBounds();
+	
+	public void setX(int x) {
+		this.x=x;
+	}
+	public int getX() {
+		return x;
+	}
+	
+	public void setY(int y) {
+		this.y=y;
+	}
+	public int getY() {
+		return y;
+	}
+	
+	public void setId(ID id) {
+		this.id=id;
+	}
+	public ID getId() {
+		return id;
+	}
+	
+	
+	public void setSpeedX(int speedx) {
+		this.spdX=speedx;
+	}
+	public int getSpeedx() {
+		return spdX;
+	}
+	
+	public void setSpeedY(int speedy) {
+		this.spdY=speedy;
+	}
+	public int getSpeedy() {
+		return spdY;
+	}
+	
+}
