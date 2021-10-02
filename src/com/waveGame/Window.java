0913bde9f8ccf11f7790f57be414377126943301
Priyanka Thakur <68439548+snippy066@users.noreply.github.com>
2021-10-02 10:14:41 +0530

    Create Window.java

---
+++ src/com/waveGame/Window.java
@@ -0,0 +1,27 @@
+package com.waveGame;
+
+import java.awt.Canvas;
+import java.awt.Dimension;
+
+import javax.swing.JFrame;
+
+public class Window extends Canvas {
+
+	private static final long serialVersionUID = 1L;
+	
+	public Window(int width, int height, String title,gameMain game) {
+		JFrame frm=new JFrame(title);
+		
+		frm.setPreferredSize(new Dimension(width,height));
+		frm.setMaximumSize(new Dimension(width,height));
+		frm.setMinimumSize(new Dimension(width,height));
+		
+		frm.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frm.setResizable(false);
+		frm.setLocationRelativeTo(null);
+		frm.add(game);
+		frm.setVisible(true);
+		game.start();
+		
+	}
+}
