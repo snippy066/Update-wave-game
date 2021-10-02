3ac02d34d4d763129caebd138a43a48ce4dda328
Priyanka Thakur <68439548+snippy066@users.noreply.github.com>
2021-10-02 10:14:36 +0530

    Create Player.java

---
+++ src/com/waveGame/Player.java
@@ -0,0 +1,62 @@
+package com.waveGame;
+
+import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.Rectangle;
+import java.util.Random;
+
+public class Player extends GameObject {
+	
+	Random r=new Random();
+	Handler handler;
+
+	public Player(int x, int y, ID id,Handler handler) {
+		super(x, y, id);
+	    this.handler=handler;
+	   
+	}
+	
+	
+
+	@Override
+	public void tick() {
+		x+=spdX;
+		y+=spdY;
+		
+		x=gameMain.clamp(x, 3, gameMain.w-48);
+		y=gameMain.clamp(y, 3, gameMain.h-72);
+		
+		 handler.addObject(new BasicTrail(x,y,Color.white,32,32,0.05f,ID.BasicTrail,handler));
+		
+		  collision();
+		  
+		
+	}
+
+	private void collision() {
+		for(int i=0;i<handler.obj.size();i++) {
+			
+			GameObject temp=handler.obj.get(i);
+			
+			if(temp.getId()== ID.BasicEnemy) {
+				if(getBounds().intersects(temp.getBounds())) {
+					HUD.health-=2;
+				}
+			}
+		}
+	}
+	@Override
+	public void render(Graphics g) {
+		g.setColor(Color.white);
+		g.fillRect(x, y,32, 32);
+		
+	}
+
+
+
+	@Override
+	public Rectangle getBounds() {
+		// TODO Auto-generated method stub
+		return new Rectangle(x,y,32,32);
+	}
+}
