f85e7a5f7f5f5a118feba9c7e7bf461cf5d3b027
Priyanka Thakur <68439548+snippy066@users.noreply.github.com>
2021-10-04 00:00:04 +0530

    Update HUD.java

--- src/com/waveGame/HUD.java
+++ src/com/waveGame/HUD.java
@@ -6,18 +6,20 @@ import java.awt.Graphics;
 public class HUD {
 	
 	public static int health=100;
+	private int greenColor=0;
 	
 	public void tick() {
 //		health--;
 		
 		health=Game.clamp(health, 0, 100);
 		
+		
 	}
 	
 	public void render(Graphics g) {
 		g.setColor(Color.gray);
 		g.fillRect(3, 3, 200, 28);
-		g.setColor(Color.green);
+		g.setColor(new Color(75,greenColor,0));
 		g.fillRect(3,3, health*2, 28);
 		g.setColor(Color.WHITE);
 		g.drawRect(3, 3, 200, 28);
