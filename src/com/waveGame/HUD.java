77d408cda94279a5446876b740dd8caff4857821
Priyanka Thakur <68439548+snippy066@users.noreply.github.com>
2021-10-04 00:42:00 +0530

    health decrement effect

--- src/com/waveGame/HUD.java
+++ src/com/waveGame/HUD.java
@@ -6,20 +6,28 @@ import java.awt.Graphics;
 public class HUD {
 	
 	public static int health=100;
-	private int greenColor=0;
+	private int greenColor=255;
 	
 	public void tick() {
 //		health--;
 		
 		health=Game.clamp(health, 0, 100);
+		greenColor=Game.clamp(greenColor,0, 255);
 		
+		
+		greenColor=health*2;
 		
 	}
 	
 	public void render(Graphics g) {
 		g.setColor(Color.gray);
 		g.fillRect(3, 3, 200, 28);
-		g.setColor(new Color(75,greenColor,0));
+		if(health>50) {
+		g.setColor(new Color(0,greenColor,0));
+		}
+		else {
+			g.setColor(new Color(greenColor+100,0,0));
+		}
 		g.fillRect(3,3, health*2, 28);
 		g.setColor(Color.WHITE);
 		g.drawRect(3, 3, 200, 28);
