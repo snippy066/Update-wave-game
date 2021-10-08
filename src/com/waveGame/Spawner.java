635df774f310c45147dc097633c4ad9bb0ee1dbc
Priyanka Thakur <68439548+snippy066@users.noreply.github.com>
2021-10-08 23:35:43 +0530

    Update Spawner.java

--- src/com/waveGame/Spawner.java
+++ src/com/waveGame/Spawner.java
@@ -5,6 +5,9 @@ public class Spawner  {
    private Handler handler;
    private HUD hud;
    
+   private int scoreKeep=0;
+   private int scoreLevel=500;
+   
    public Spawner(Handler handler,HUD hud) {
 	   this.handler=handler;
 	   this.hud=hud;
@@ -12,6 +15,13 @@ public class Spawner  {
    }
    
    public void tick() {
+	    scoreKeep++;
 	    
+	    if(scoreKeep>=scoreLevel) {
+	    	scoreKeep=0;
+	    	scoreLevel+=200;
+	    	hud.setLevel(hud.getLevel()+1);
+	    	System.out.println(hud.getLevel());
+	    }
    }
 }
