186ee93c7fadba59cbc7cd6b806f2ed1f3d04edf
Priyanka Thakur <68439548+snippy066@users.noreply.github.com>
2021-10-11 16:59:07 +0530

    creating a boss enemy

---
+++ src/com/waveGame/BossEnemy.java
@@ -0,0 +1,46 @@
+package com.waveGame;
+
+import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.Rectangle;
+
+public class BossEnemy extends GameObject{
+	private Handler handler;
+
+	public BossEnemy(int x, int y, ID id,Handler handler) {
+		super(x, y, id);
+		this.handler=handler;
+		
+		spdX=0;
+		spdY=5;
+	}
+
+	@Override
+	public void tick() {
+		// TODO Auto-generated method stub
+		x+=spdX;
+		y+=spdY;
+		
+		
+		if(y<=0 || y>=Game.h-32)  spdY*=-1;
+		if(x<=0 || x>=Game.w-64)  spdX*=-1;
+		
+		handler.addObject(new BasicTrail(x,y,Color.red,16,16,0.01f,ID.BasicTrail,handler));
+		
+	}
+
+	@Override
+	public void render(Graphics g) {
+		// TODO Auto-generated method stub
+		g.setColor(Color.red);
+		g.fillRect(x,y,16,16);
+		
+	}
+
+	@Override
+	public Rectangle getBounds() {
+		// TODO Auto-generated method stub
+		return new Rectangle(x,y,16,16);
+	}
+
+}
